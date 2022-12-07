183b44854dfa6bb519f70d350c9f5c2b1f81de3d
AnshumaanTanwar <tanwaranshumaan@gmail.com>
2022-12-08 02:05:04 +0530

    defined tables

---
+++ create_tables.py
@@ -0,0 +1,26 @@
+import mysql.connector as conn
+
+raildb = conn.connect(host='localhost', user='root', password='root', database='railway')
+
+cursor = raildb.cursor()
+
+cursor.execute("""CREATE TABLE trains(
+    tname VARCHAR(20),
+    tnum INT,
+    AC1 INT,
+    AC2 INT,
+    AC3 INT,
+    slp INT
+)""")
+
+cursor.execute("""CREATE TABLE passengers(
+    pname VARCHAR(20),
+    age INT,
+    trainnum INT,
+    pnums INT,
+    class VARCHAR(3),
+    ticket_price INT,
+    pnr INT PRIMARY KEY,
+    status VARCHAR(10)
+    )""")
+
